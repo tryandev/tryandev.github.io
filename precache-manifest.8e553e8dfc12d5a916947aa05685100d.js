ff2ccbcd0d1a2b8a77348ae09618ed95125b9d4b
ryan tan <cloudzerox.tan@gmail.com>
2019-09-03 03:53:45 -0700

    Updates

---
+++ precache-manifest.8e553e8dfc12d5a916947aa05685100d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1938e0b340339aa794cecd92dce7dbf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b1dc2388502d9b6156",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "21d374903c7e5529b265",
+    "url": "/static/js/2.4b1ba94f.chunk.js"
+  },
+  {
+    "revision": "f6b1dc2388502d9b6156",
+    "url": "/static/js/main.73f98214.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
